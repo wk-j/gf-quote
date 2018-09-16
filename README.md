c0097d85bc7ca24065afffa59645ed8c7b93b6ad
wk-j <somnuk.wk@gmail.com>
2018-09-16 14:14:35 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Show GreatFriends's Quote
+
+https://greatfriends06.azurewebsites.net/api/quotes
+
+## Intallation
+
+```bash
+dotnet tool install -g wk.GfQuote
+```
+
+## Usage
+
+```bash
+gf-quote
+```
---
+++ src/GfQuote/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace GfQuotes {
+    class Program {
+        static async Task Main(string[] args) {
+            var quoteUrl = "https://greatfriends06.azurewebsites.net/api/quotes";
+            using (var client = new HttpClient()) {
+                var quote = await client.GetAsync(quoteUrl);
+                var message = await quote.Content.ReadAsStringAsync();
+                var lines = message
+                    .Split(new string[] { "\\n" }, StringSplitOptions.None)
+                    .Select(x => x.Trim('"')).ToList();
+                lines.ForEach(Console.WriteLine);
+            }
+        }
+    }
+}
